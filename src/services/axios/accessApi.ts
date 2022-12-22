713d7e74f0a0d4b3fda1dc744683f887c9a3c922
josuenm <direct.josue@gmail.com>
2022-12-22 16:47:29 -0300

    feat: signUp page and authentication is done

--- src/services/axios/accessApi.ts
+++ src/services/axios/accessApi.ts
@@ -1,10 +1,15 @@
-import { SignInProps } from "@/@types/userType";
+import { SignInProps, SignUpProps } from "@/@types/userType";
 import { api } from "./api";
 
 export const accessApi = {
   signIn: async (data: SignInProps) => {
     return await api
       .post("/dashboard/signIn", data)
+      .catch((err) => err.response);
+  },
+  signUp: async (data: SignUpProps) => {
+    return await api
+      .post("/dashboard/signUp", data)
       .catch((err) => err.response);
   },
 };
---
+++ src/views/signup/index.tsx
@@ -0,0 +1,133 @@
+import { SignUpProps } from "@/@types/userType";
+import { NormalButton, OutlineButton } from "@/components/buttons";
+import { Logo } from "@/components/header";
+import { useAccess } from "@/contexts/accessContext";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { ReactNode, useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    name: yup.string().required(),
+    email: yup.string().required(),
+    password: yup.string().required(),
+  })
+  .required();
+
+export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpProps>({
+    resolver: yupResolver(schema),
+  });
+
+  const { signUp } = useAccess();
+
+  const onSubmit = (data: SignUpProps) => signUp(data);
+
+  const passwordRef = useRef<HTMLDivElement>(null);
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => setShowPassword(!showPassword);
+
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (passwordRef.current && !passwordRef.current.contains(event.target)) {
+        passwordRef.current?.classList.remove("border-primary");
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [passwordRef]);
+
+  return (
+    <main className="w-full min-h-screen flex justify-center items-center">
+      <form
+        className="w-full max-w-xs md:max-w-sm bg-white rounded-lg p-5 md:py-8 md:px-10 flex flex-col gap-10"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <header className="flex justify-center">
+          <Logo classMain="text-2xl" classPoint="text-2xl" />
+        </header>
+
+        <div className="flex flex-col gap-3">
+          <FormGroup>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              className="form-input"
+              placeholder="Type your name"
+              {...register("name")}
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm">{errors.name?.message}</p>
+            )}
+          </FormGroup>
+          <FormGroup>
+            <label htmlFor="email">E-mail:</label>
+            <input
+              type="email"
+              id="email"
+              className="form-input"
+              placeholder="Type your email"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email?.message}</p>
+            )}
+          </FormGroup>
+          <FormGroup>
+            <label htmlFor="password">Password:</label>
+
+            <div
+              className="relative form-input overflow-hidden"
+              ref={passwordRef}
+            >
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                className="outline-none"
+                placeholder="Type your password"
+                onFocus={() =>
+                  passwordRef.current?.classList.add("border-primary")
+                }
+                {...register("password")}
+              />
+
+              <div
+                className="absolute bottom-2/4 translate-y-2/4 right-0 px-3 text-lg text-primary shadow h-full flex justify-center items-center cursor-pointer"
+                onClick={togglePassword}
+              >
+                {showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+              </div>
+            </div>
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password?.message}</p>
+            )}
+          </FormGroup>
+        </div>
+
+        <footer className="flex flex-col gap-2">
+          <NormalButton type="submit">Sign Up</NormalButton>
+          <Link to="/signin">
+            <OutlineButton type="button">I have an account</OutlineButton>
+          </Link>
+        </footer>
+      </form>
+    </main>
+  );
+}
+
+function FormGroup({ children }: { children?: ReactNode | ReactNode[] }) {
+  return <div className="flex flex-col gap-1">{children}</div>;
+}
