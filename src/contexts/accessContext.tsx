b3f619d7dd7e574b1145040f0427241c8f14add8
josuenm <direct.josue@gmail.com>
2022-12-22 16:15:56 -0300

    feat: sign out is working well

---
+++ src/contexts/accessContext.tsx
@@ -0,0 +1,60 @@
+import { SignInProps } from "@/@types/userType";
+import { accessApi } from "@/services/axios/accessApi";
+import { destroyCookie, setCookie } from "nookies";
+import { createContext, ReactNode, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useGlobalTools } from "./globalToolsContext";
+
+interface ContextProps {
+  signIn: (data: SignInProps) => Promise<void>;
+  signUp: () => Promise<void>;
+  signOut: () => void;
+}
+
+interface ProviderProps {
+  children: ReactNode | ReactNode[];
+}
+
+const Context = createContext({} as ContextProps);
+
+export function AccessContextProvider({ children }: ProviderProps) {
+  const navigate = useNavigate();
+
+  const { successNotification, errorNotification, fetchLoadingScreen } =
+    useGlobalTools();
+
+  const signIn = async (data: SignInProps) => {
+    fetchLoadingScreen(true);
+    const res = await accessApi.signIn(data);
+    fetchLoadingScreen(false);
+
+    switch (res.status) {
+      case 200:
+        setCookie(res.data.token, "jodash.token", "value", {
+          path: "/",
+        });
+        successNotification("Successful Login");
+        navigate("/");
+        break;
+
+      default:
+        errorNotification("Somenthing wrong, try again");
+        break;
+    }
+  };
+
+  const signUp = async () => {};
+
+  const signOut = () => {
+    destroyCookie(null, "jodash.token");
+    navigate("/signIn");
+  };
+
+  return (
+    <Context.Provider value={{ signIn, signUp, signOut }}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+export const useAccess = () => useContext(Context);
